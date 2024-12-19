6ab388c048890667836f39553b33dc36f246341c
Fabrice Senecal <2042108@cstjean.qc.ca>
2024-12-19 13:26:56 -0500

    Envoie du projet démonstratif

--- TP1Restaurant/app/build.gradle.kts
+++ TP1Restaurant/app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
 
     implementation("androidx.navigation:navigation-fragment-ktx:2.8.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.8.3")
+
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
@@ -53,22 +54,18 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.room:room-common:2.6.1")
     implementation("androidx.room:room-runtime:2.6.1")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.3")
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.6")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6")
     implementation("androidx.room:room-ktx:2.6.1")
     implementation("androidx.cardview:cardview:1.0.0")
     kapt("androidx.room:room-compiler:2.6.1")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.3")
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.3")
 
     implementation("androidx.compose.ui:ui:1.7.4")
     implementation("androidx.compose.material:material:1.7.4")
     implementation("androidx.compose.ui:ui-tooling-preview:1.7.4")
     implementation("androidx.compose.runtime:runtime:1.7.4")
     implementation("androidx.activity:activity-compose:1.9.3")
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
